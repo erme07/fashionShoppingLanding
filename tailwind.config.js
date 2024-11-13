65cbfa7116665047be8573f1756da69622b9c145
erme07 <erik.medina07@gmail.com>
2024-11-13 16:51:09 -0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,8 @@ module.exports = {
         'Roboto' : ['Roboto', 'sans-serif']
       },
       backgroundImage: {
-        'payday': "url('/assets/img/payfinal.jpg')",
-        'hero': "url('/assets/img/hero.png')",
+        'payday': "url('/assets/img/pay.webp')",
+        'hero': "url('/assets/img/hero.webp')",
       },
       dropShadow: {
         'app': '0px 4px 60px rgba(0, 0, 0, 0.25)',
@@ -24,11 +24,22 @@ module.exports = {
       },
       keyframes: {
         move: {
-          'from': { transform: 'translateX(0)' },
-          'to': { transform: 'translateX(15px)' }
-        }
+          'from': { transform: 'translate(0px, -50%)' },
+          'to': { transform: 'translate(15px, -50%)' }
+        },
+        'wave-medium': {
+          '0%': { width: '300px', height: '300px', opacity: '1' },
+          '50%': { opacity: '1' },
+          '100%': { width: '620px', height: '620px', opacity: '0' },
+        },
+        'wave-big': {
+          '0%': { width: '420px', height: '420px', opacity: '1' },
+          '50%': { opacity: '1' },
+          '100%': { width: '740px', height: '740px', opacity: '0' },
+        },
       },
       animation: {
+        'spin-slow': 'spin 5s linear infinite',
         'move-slow': 'move .5s infinite alternate',
       }
     },
@@ -36,6 +47,8 @@ module.exports = {
       center: true,
       padding: {
         DEFAULT: '10px',
+        '2xl':'30px',
+        'xl':'20px',
       }
     },
   },
